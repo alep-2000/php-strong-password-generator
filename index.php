43372f57d01ce5302b93f4cd63bbb7fc7e824164
alep-2000 <alessiopiras056@gmail.com>
2023-07-06 21:07:32 +0200

    [F][L]Milestone 1 completed

--- index.php
+++ index.php
@@ -1,4 +1,44 @@
 <?php 
+// FUNCTION TO GENERATE RANDOM NUMBER
+function randomNumber($min, $max){
+    return rand($min, $max);
+}
+
+// FUNCTION TO GENERATE RANDOM PASSWORD
+function randomPassword ($lenght){
+    // VARIABLES
+    $password = '';
+    $error = '';
+    $minuscole = 'abcdefghijklmnopqrstuvwxyz';
+    $maiuscole = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $numbers = '1234567890';
+    $special = '!?~@#-_+<>[]{}';
+
+    $string = $minuscole.$maiuscole.$numbers.$special;
+
+    if($lenght === ''){
+        $error = 'Non hai inserito nulla';
+    }
+    else if($lenght < 8 || $lenght > 32){
+        $results = 'Password troppo corta';
+    }
+    else{
+        // CICLE WHILE
+        while(strlen($password) < $lenght){
+            $index = randomNumber(0, strlen($string) - 1);
+            $characater = $string[$index];
+
+            $password .= $characater;
+        }
+        return $password;
+    }   
+    return $results;
+}
+
+
+if(isset ($_GET['lenght'])){
+    $results = randomPassword($_GET['lenght']);
+}
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +49,14 @@
     <title> PHP Strong Password Generator</title>
 </head>
 <body>
-    
+    <?php if(isset ($results)){ ?>
+        <div>
+            <?php echo $results; ?>
+        </div>
+    <?php } ?>
+    <form action="index.php" method="GET">
+        <input type="number" name="lenght" placeholder="Lenght password">
+        <button type="submit">Invia</button>
+    </form>
 </body>
 </html>
